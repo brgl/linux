308e8bbd1dfb8419fcfb13eef725c7c7ebb55c36
Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
2025-11-21 14:04:30 +0100

    gpio: shared: handle the reset-gpios corner case
    There's an unexpected interaction between the reset-gpio driver and the
    shared GPIO support. The reset-gpio device is an auxiliary device that's
    created dynamically and fulfills a similar role to the gpio-shared-proxy
    driver but is limited in scope to just supporting the "reset-gpios"
    property.
    
    The shared GPIO core code does not take into account that the machine
    lookup entry we create when scanning the device-tree must connect the
    reset-gpio device - that is the actual consumer of the GPIO and not the
    consumer defined on the device tree, which in turn consumes the shared
    reset control exposed by the reset-gpio device - to the GPIO controller.
    
    We also must not skip the gpio-shared-proxy driver as it's possible that
    a shared GPIO may be used by one consumer as a reset-gpios going through
    the reset-gpio device and another that uses GPIOLIB.
    
    We need to make it a special case handled in gpiolib-shared.c. Add a new
    function - gpio_shared_dev_is_reset_gpio() - whose role it is to verify
    if a non-matching consumer of a shared pin is a reset-gpio device and
    make sure it's the right one for this pin. To that end make sure that
    its parent is the GPIO controller in question and that the fwnode we
    identified as sharing the pin references that controller via the
    "reset-gpios" property.
    
    Only include that code if the reset-gpio driver is enabled.
    
    Fixes: a060b8c511ab ("gpiolib: implement low-level, shared GPIO support")
    Reported-by: Val Packett <val@packett.cool>
    Closes: https://lore.kernel.org/all/3b5d9df5-934d-4591-8827-6c9573a6f7ba@packett.cool/
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>

--- drivers/gpio/gpiolib-shared.c
+++ drivers/gpio/gpiolib-shared.c
@@ -253,6 +253,66 @@ static int gpio_shared_make_adev(struct gpio_device *gdev,
 	return 0;
 }
 
+#if IS_ENABLED(CONFIG_RESET_GPIO)
+/*
+ * Special case: reset-gpio is is an auxiliary device that's created
+ * dynamically and put in between the GPIO controller and consumers of shared
+ * GPIOs referred to by the "reset-gpios" property.
+ *
+ * If the supposed consumer of a shared GPIO didn't match any of the mappings
+ * we created when scanning the firmware nodes, it's still possible that it's
+ * the reset-gpio device which didn't exist at the time of the scan.
+ *
+ * This function verifies it an return true if it's the case.
+ */
+static bool gpio_shared_dev_is_reset_gpio(struct device *consumer,
+					  struct gpio_shared_entry *entry,
+					  struct gpio_shared_ref *ref)
+{
+	struct device *parent = consumer->parent;
+	struct fwnode_handle *remote;
+	bool ret;
+
+	if (!parent)
+		return false;
+
+	/*
+	 * FIXME: use device_is_compatible() once the reset-gpio drivers gains
+	 * a compatible string which it currently does not have.
+	 */
+	if (!strstarts(dev_name(consumer), "reset.gpio."))
+		return false;
+
+	/*
+	 * Parent of the reset-gpio auxiliary device is the GPIO chip whose
+	 * fwnode we stored in the entry structure.
+	 */
+	if (!device_match_fwnode(parent, entry->fwnode))
+		return false;
+
+	/*
+	 * The device associated with the shared reference's firmware node is
+	 * the consumer of the reset control exposed by the reset-gpio device.
+	 * It must have a "reset-gpios" property that's referencing the entry's
+	 * firmware node.
+	 */
+	remote = fwnode_find_reference(ref->fwnode, "reset-gpios", 0);
+	if (IS_ERR(remote))
+		return false;
+
+	ret = (remote == entry->fwnode);
+	fwnode_handle_put(remote);
+	return ret;
+}
+#else
+static bool gpio_shared_dev_is_reset_gpio(struct device *consumer,
+					  struct gpio_shared_entry *entry,
+					  struct gpio_shared_ref *ref)
+{
+	return false;
+}
+#endif /* CONFIG_RESET_GPIO */
+
 int gpio_shared_add_proxy_lookup(struct device *consumer, unsigned long lflags)
 {
 	const char *dev_id = dev_name(consumer);
@@ -268,7 +328,8 @@ int gpio_shared_add_proxy_lookup(struct device *consumer, unsigned long lflags)
 
 	list_for_each_entry(entry, &gpio_shared_list, list) {
 		list_for_each_entry(ref, &entry->refs, list) {
-			if (!device_match_fwnode(consumer, ref->fwnode))
+			if (!device_match_fwnode(consumer, ref->fwnode) &&
+			    !gpio_shared_dev_is_reset_gpio(consumer, entry, ref))
 				continue;
 
 			/* We've already done that on a previous request. */
