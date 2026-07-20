f0808c79dac7fb0fbb868120cbcf51fd253a7d7d
Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>
2026-07-20 10:12:59 +0200

    mfd: tps6586: use platform_device_set_fwnode()
    Prefer the higher-level platform_device_set_fwnode() over the
    OF-specific platform_device_set_of_node() for dynamically allocated
    platform devices.
    
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>

--- drivers/mfd/tps6586x.c
+++ drivers/mfd/tps6586x.c
@@ -397,7 +397,7 @@ static int tps6586x_add_subdevs(struct tps6586x *tps6586x,
 
 		pdev->dev.parent = tps6586x->dev;
 		pdev->dev.platform_data = subdev->platform_data;
-		platform_device_set_of_node(pdev, subdev->of_node);
+		platform_device_set_fwnode(pdev, of_fwnode_handle(subdev->of_node));
 
 		ret = platform_device_add(pdev);
 		if (ret) {
